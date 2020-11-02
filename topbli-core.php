c85ac15a0036f58507681cec8531bd0fa732dcef
Hengky <kiky1991@gmail.com>
2020-11-02 13:05:41 +0700

    Fresh commit Plugin

---
+++ topbli-core.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Plugin Name:     Topbli Dropship
+ * Plugin URI:      https://www.cybersoftmedia.com
+ * Description:     Topbli Dropship is dropship information for topbli.com site
+ * Version:         1.0.0
+ * Author:          Hengky ST
+ * Author URI:      https://www.cybersoftmedia.com
+ * License:         GPL
+ * Text Domain:     topdrop
+ * Domain Path:     /languages
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
