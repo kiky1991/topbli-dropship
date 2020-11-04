763e05ef8a5e9407e09f6518f2d59c2e412d64e0
Hengky <kiky1991@gmail.com>
2020-11-04 09:11:15 +0700

    Fixed css UX 2

--- views/admin-edit-shop.php
+++ views/admin-edit-shop.php
@@ -1,7 +1,7 @@
-<p class="form-field form-field-wide wc-dropshipping" style="margin-top:50px">&nbsp;</p>
+<p class="form-field form-field-wide topdrop-dropshipping">&nbsp;</p>
 <h3><?php esc_html_e('Dropshipping', 'topdrop'); ?></h3>
 <div class="form-dropship-information">
-    <p class="form-field form-field-wide validate-required" id="topdrop_name_field" data-priority="">
+    <p class="form-field form-field-wide topdrop-name" id="topdrop_name_field" data-priority="">
         <label for="topdrop_name">Name:
             <?php if ($user_id > 0 || (!empty($screen->action && $screen->action === 'add'))) : ?>
                 &nbsp;
@@ -13,7 +13,7 @@
         <span class="woocommerce-input-wrapper">
             <input type="text" class="input-text " name="topdrop_name" id="topdrop_name" placeholder="" value="<?php esc_attr_e($dropship_name, 'topdrop'); ?>"></span>
     </p>
-    <p class="form-field form-field-wide" id="topdrop_phone_field" data-priority="">
+    <p class="form-field form-field-wide topdrop-phone" id="topdrop_phone_field" data-priority="">
         <label for="topdrop_phone" class="">Phone:</label>
         <span class="woocommerce-input-wrapper">
             <input type="tel" class="input-text" name="topdrop_phone" id="topdrop_phone" placeholder="" value="<?php esc_attr_e($dropship_phone, 'topdrop'); ?>">
